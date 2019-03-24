e1bfd4f9b93a9af537ce9a83d22ad370d048381a
stormy377 <dan4ik377@gmail.com>
2019-03-24 11:55:17 +0100

    Fixing Bugs

--- src/parts/modal.js
+++ src/parts/modal.js
@@ -1,30 +1,5 @@
 function modal() {
-
-    // const overlay = document.querySelector(".overlay");
-
-    // function showModal(modBtn) {
-    //     overlay.style.display = "block";
-    //     info.classList.add("more-splash");
-    //     document.body.style.overflow = "hidden";
-    // }
-
-    // function hideModal(modCloseBtn) {
-    //     overlay.style.display = "none";
-    //     info.classList.remove("more-splash");
-    //     document.body.style.overflow = "";
-    // }
-
-    // if (target && target.classList.contains("more")) {
-    //     showModal(target);
-    // }
-    // if (target && target.classList.contains("popup-close")) {
-    //     hideModal(target);
-    // }
-    // if (target && target.classList.contains("description-btn")) {
-    //     showModal(target);
-    // }
-
-
+    
     const more = document.querySelectorAll('.more, .description-btn'),
     overlay = document.querySelector('.overlay'),
     close = document.querySelector('.popup-close');
